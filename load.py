b4498c09ded834a7b2cf56d3c9f765697a9fe468
Aslı Sabancı <aslisabanci@gmail.com>
2021-03-09 11:06:09 -0800

    Made algo version parametric

--- load.py
+++ load.py
@@ -3,20 +3,23 @@ import pandas as pd
 import sys
 
 if __name__ == "__main__":
-    to_load = "all"
-    args = sys.argv[1:]
-    if len(args):
-        to_load = args[0]
+    while True:
+        to_load = "all"
+        version = "0.1.1"
+        args = sys.argv[1:]
+        if len(args):
+            to_load = args[0]
+            version = args[1]
 
-    input_ds = pd.read_csv(f"./data/{to_load}.csv")
-    print(input_ds.shape)
+        input_ds = pd.read_csv(f"./data/{to_load}.csv")
+        print(input_ds.shape)
 
-    client = Algorithmia.client(
-        "simqUvvKryCWCWgjihWcuGv7fro1", "https://api.therealreal.productionize.ai"
-    )
-    algo = client.algo("asli_algorithmia_trr/credit_card_approval/0.1.1")
+        client = Algorithmia.client(
+            "simqUvvKryCWCWgjihWcuGv7fro1", "https://api.therealreal.productionize.ai"
+        )
+        algo = client.algo(f"asli_algorithmia_trr/credit_card_approval/{version}")
 
-    records = input_ds.to_dict("records")
-    for record in records:
-        result = algo.pipe(record).result
-        print(result)
+        records = input_ds.to_dict("records")
+        for record in records:
+            result = algo.pipe(record).result
+            print(f"{result} from v{version}")
