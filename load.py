e1aaf1e24bd055b122c613b36c86d660e9eb3d2d
Aslı Sabancı <aslisabanci@gmail.com>
2021-03-09 18:19:54 -0800

    Made owns_home and has_work_phone parametric

--- load.py
+++ load.py
@@ -6,10 +6,14 @@ if __name__ == "__main__":
     while True:
         to_load = "all"
         version = "0.1.1"
+        owns_home = "-"
+        has_work_phone = "-"
         args = sys.argv[1:]
         if len(args):
             to_load = args[0]
             version = args[1]
+            owns_home = args[2]
+            has_work_phone = args[3]
 
         input_ds = pd.read_csv(f"./data/{to_load}.csv")
         print(input_ds.shape)
@@ -21,5 +25,19 @@ if __name__ == "__main__":
 
         records = input_ds.to_dict("records")
         for record in records:
+            if owns_home == "1":
+                record["owns_home"] = 1
+            elif owns_home == "0":
+                record["owns_home"] = 0
+
+            if has_work_phone == "1":
+                record["has_work_phone"] = 1
+            elif has_work_phone == "0":
+                record["has_work_phone"] = 0
+
             result = algo.pipe(record).result
-            print(f"{result} from v{version}")
+            owns_home_modified = record["owns_home"]
+            has_work_phone_modified = record["has_work_phone"]
+            print(
+                f"{result} from v{version}, {owns_home_modified}, {has_work_phone_modified}"
+            )
