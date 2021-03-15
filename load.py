fde8d8192b04c71cca5acd1b78cfc0b863d96255
Aslı Sabancı <aslisabanci@gmail.com>
2021-03-15 10:40:10 -0700

    Use argparse for maintainability

--- load.py
+++ load.py
@@ -1,43 +1,52 @@
 import Algorithmia
 import pandas as pd
-import sys
+import argparse
 
-if __name__ == "__main__":
-    while True:
-        to_load = "all"
-        version = "0.1.1"
-        owns_home = "-"
-        has_work_phone = "-"
-        args = sys.argv[1:]
-        if len(args):
-            to_load = args[0]
-            version = args[1]
-            owns_home = args[2]
-            has_work_phone = args[3]
+argparser = argparse.ArgumentParser()
+argparser.add_argument(
+    "-s",
+    action="store",
+    default="all",
+    help="Data segment to use:all, approve or reject",
+)
+argparser.add_argument(
+    "-v", action="store", default="0.1.1", help="Algorithm version to call"
+)
+argparser.add_argument(
+    "-home", action="store", default=None, help="Sets owns_home input feature to 1 or 0"
+)
+argparser.add_argument(
+    "-phone",
+    action="store",
+    default=None,
+    help="Sets has_work_phone input feature to 1 or 0",
+)
 
-        input_ds = pd.read_csv(f"./data/{to_load}.csv")
-        print(input_ds.shape)
+args = argparser.parse_args()
+print(vars(args))
 
-        client = Algorithmia.client(
-            "simqUvvKryCWCWgjihWcuGv7fro1", "https://api.therealreal.productionize.ai"
-        )
-        algo = client.algo(f"asli_algorithmia_trr/credit_card_approval/{version}")
+while True:
+    to_load = args.s
+    version = args.v
+    owns_home = args.home
+    has_work_phone = args.phone
 
-        records = input_ds.to_dict("records")
-        for record in records:
-            if owns_home == "1":
-                record["owns_home"] = 1
-            elif owns_home == "0":
-                record["owns_home"] = 0
+    input_ds = pd.read_csv(f"./data/{to_load}.csv")
+    print(input_ds.shape)
 
-            if has_work_phone == "1":
-                record["has_work_phone"] = 1
-            elif has_work_phone == "0":
-                record["has_work_phone"] = 0
+    client = Algorithmia.client(
+        "simqUvvKryCWCWgjihWcuGv7fro1", "https://api.therealreal.productionize.ai"
+    )
+    algo = client.algo(f"asli_algorithmia_trr/credit_card_approval/{version}")
 
-            result = algo.pipe(record).result
-            owns_home_modified = record["owns_home"]
-            has_work_phone_modified = record["has_work_phone"]
-            print(
-                f"{result} from v{version}, {owns_home_modified}, {has_work_phone_modified}"
-            )
+    records = input_ds.to_dict("records")
+    for record in records:
+        if owns_home is not None:
+            record["owns_home"] = owns_home
+        if has_work_phone is not None:
+            record["has_work_phone"] = has_work_phone
+
+        result = algo.pipe(record).result
+        record_owns_home = record["owns_home"]
+        record_has_work_phone = record["has_work_phone"]
+        print(f"{result} from v{version}, {record_owns_home}, {record_has_work_phone}")
